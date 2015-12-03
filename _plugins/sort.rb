e9d98563c6465e06565ce5b027da5cf568186245
Phil Sturgeon <me@philsturgeon.uk>
2015-12-03 16:27:02 -0500

    Fixed PHP 7 page

--- _plugins/sort.rb
+++ _plugins/sort.rb
@@ -3,8 +3,21 @@ require 'semantic'
 module Jekyll
   module SortFilter
     def semver_sort_by(input, property = nil)
-      ary = [input].flatten
-      ary.sort {|a,b| Semantic::Version.new(a[property]) <=> Semantic::Version.new(b[property]) }
+      Array(input).sort do |a,b|
+        Semantic::Version.new(a[property]) <=> Semantic::Version.new(b[property])
+      end
+    end
+
+    def filter_final_only(input, property = nil)
+      Array(input).select { |i| i[property].is_a? ::Integer }
+    end
+
+    def filter_dev_only(input, property = nil)
+      Array(input).select { |i| i[property].is_a? ::String }
+    end
+
+    def is_inty?(version)
+       !!(version =~ /\A[-+]?[0-9]+\z/)
     end
   end
 end

4c89a26233fe2ad33a842bfeb1da8309f1e05068
matt trask <mjftrask@gmail.com>
2019-01-12 18:20:00 -0600

    last changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container mx-auto">
     <div class="flex justify-center">
-        <router-link :to="{name: 'Home'}"><img src="/images/php-versions.png" class="h-64 w-64" alt="PHPVersions.info logo"></router-link>
+        <router-link :to="{name: 'Home'}"><img src="/images/phpversions.png" class="h-64 w-64" alt="PHPVersions.info logo"></router-link>
     </div>
     <router-view></router-view>
 </div>

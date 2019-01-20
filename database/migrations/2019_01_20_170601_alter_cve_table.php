050dc6fc63b2e2c79940a1a307f10a34aab24010
matt trask <mjftrask@gmail.com>
2019-01-20 11:08:12 -0600

    alter cve table to make cve_id a string

---
+++ database/migrations/2019_01_20_170601_alter_cve_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCveTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE `vulnerabilities` CHANGE  `cve_id` VARCHAR(45);');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('ALTER TABLE `vulnerabilities` CHANGE  `cve_id` INT(11);');
+    }
+}

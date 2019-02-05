46b8f8dd0b981f76da718de3ee09c616e2e5e13e
matt trask <mjftrask@gmail.com>
2019-02-04 21:37:29 -0600

    add fathom for analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,21 @@
             'csrfToken' => csrf_token(),
         ]); ?>
     </script>
+    <!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
+    <script>
+      (function(f, a, t, h, o, m){
+        a[h]=a[h]||function(){
+            (a[h].q=a[h].q||[]).push(arguments)
+          };
+        o=f.createElement('script'),
+          m=f.getElementsByTagName('script')[0];
+        o.async=1; o.src=t; o.id='fathom-script';
+        m.parentNode.insertBefore(o,m)
+      })(document, window, '//157.230.91.29/tracker.js', 'fathom');
+      fathom('set', 'siteId', 'TIVQG');
+      fathom('trackPageview');
+    </script>
+    <!-- / Fathom -->
 </head>
 <body>
     <div id="app">

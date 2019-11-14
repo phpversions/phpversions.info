99f1e1097b771ff4a85867832108d0110e89165a
Jachim Coudenys <jachim.coudenys@combellgroup.com>
2019-11-14 10:23:08 +0100

    Add 7.4 to the list

--- _plugins/sort.rb
+++ _plugins/sort.rb
@@ -11,6 +11,7 @@ module Jekyll
     def semver_sort(hosts)
       Array(hosts).sort do |a,b|
 
+        a74 = a['versions'][74] && a['versions'][74]['semver'] ? Semantic::Version.new(a['versions'][74]['semver']) : '0.0.1'
         a73 = a['versions'][73] && a['versions'][73]['semver'] ? Semantic::Version.new(a['versions'][73]['semver']) : '0.0.1'
         a72 = a['versions'][72] && a['versions'][72]['semver'] ? Semantic::Version.new(a['versions'][72]['semver']) : '0.0.1'
         a71 = a['versions'][71] && a['versions'][71]['semver'] ? Semantic::Version.new(a['versions'][71]['semver']) : '0.0.1'
@@ -20,6 +21,7 @@ module Jekyll
         a54 = a['versions'][54] && a['versions'][54]['semver'] ? Semantic::Version.new(a['versions'][54]['semver']) : '0.0.1'
         adefault = a['default'] ? a['default'].to_i : 0
 
+        b74 = b['versions'][74] && b['versions'][74]['semver'] ? Semantic::Version.new(b['versions'][74]['semver']) : '0.0.1'
         b73 = b['versions'][73] && b['versions'][73]['semver'] ? Semantic::Version.new(b['versions'][73]['semver']) : '0.0.1'
         b72 = b['versions'][72] && b['versions'][72]['semver'] ? Semantic::Version.new(b['versions'][72]['semver']) : '0.0.1'
         b71 = b['versions'][71] && b['versions'][71]['semver'] ? Semantic::Version.new(b['versions'][71]['semver']) : '0.0.1'
@@ -29,7 +31,7 @@ module Jekyll
         b54 = b['versions'][54] && b['versions'][54]['semver'] ? Semantic::Version.new(b['versions'][54]['semver']) : '0.0.1'
         bdefault = b['default'] ? b['default'].to_i : 0
 
-        [b73, b72, b71, bdefault, b70, b56, b55, b54] <=> [a73, a72, a71, adefault, a70, a56, a55, a54]
+        [b74, b73, b72, b71, bdefault, b70, b56, b55, b54] <=> [a74, a73, a72, a71, adefault, a70, a56, a55, a54]
       end
     end
 
